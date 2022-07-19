9ac8a689122a5e41092ce5be7896d108cd59a55e
Diego Augusto <DiegoprogBr@hotmail.com>
2022-07-19 19:16:06 -0300

    Exercício explosivo de python.

---
+++ explosion.py
@@ -0,0 +1,18 @@
+import time
+
+def explosion():
+    print("Iniciando contagem regressiva!")
+    time.sleep(1)
+    print(5)
+    time.sleep(2)
+    print(4)
+    time.sleep(3)
+    print(3)
+    time.sleep(4)
+    print(2)
+    time.sleep(5)
+    print(1)
+    time.sleep(6)
+    print("Buuuummmm!!!!")
+
+explosion()
